cf4727f3d1f1f0d663e34cdd0e2b5a897183a447
nikalosa <nikalosaberidze@gmail.com>
2020-08-30 02:51:20 +0400

    Maximum scorers for each original voices query implementation

--- functions/queries/queries.py
+++ functions/queries/queries.py
@@ -1,6 +1,7 @@
 import json
 from google.cloud import bigquery
 
+
 def voice_average_score():
     client = bigquery.Client('speech-similarity')
 
@@ -24,3 +25,54 @@ def voice_average_score():
 
     return json.dumps(result)
 
+
+def voice_maximum_scorers():
+    # TOP = 3
+    client = bigquery.Client('speech-similarity')
+
+    query_job = client.query(
+        """
+            with
+              cte 
+            as
+            ( 
+              select 
+                OriginalVoiceId, UserId, Score, ROW_NUMBER() 
+              over 
+              (
+                partition by 
+                  OriginalVoiceId
+                order by 
+                  Score 
+                desc
+              )
+              as 
+                rn
+              from 
+                statistics.test
+            )
+            select 
+              OriginalVoiceId, UserId, Score, rn
+            from 
+              cte
+            where 
+              rn <= 3
+            order by 
+              OriginalVoiceId, rn 
+        """
+
+    )
+
+    max_scorers = query_job.result()
+    result = {}
+
+    for row in max_scorers:
+        orig = row.OriginalVoiceId
+        if orig not in result:
+            result[orig] = []
+        result[orig].append({
+            'UserId' : row.UserId,
+            'Score': row.Score,
+        })
+
+    return json.dumps(result)
