df2e6d170caca7ff2c9a817aad29d56c48a85dcd
nikalosa <nikalosaberidze@gmail.com>
2020-08-30 19:40:42 +0400

    Format

--- functions/voice/main.py
+++ functions/voice/main.py
@@ -8,7 +8,6 @@ def get_all_voice_statistics(request):
     max_scorers = queries.voice_maximum_scorers()
 
     result = {}
-
     for voice_id in users_tried.keys():
         statistics = {'usersTried': users_tried[voice_id],
                       'averageScorers': average_scores[voice_id],
@@ -25,9 +24,6 @@ def get_one_voice_statistics(request):
         voice_id = request_json['originalVoiceId']
     elif request_args and 'originalVoiceId' in request_args:
         voice_id = int(request_args['originalVoiceId'])
-    else:
-        # return error apiresponse
-        return ""
 
     result = {}
     result.update(queries.voice_users_tried()[voice_id])
