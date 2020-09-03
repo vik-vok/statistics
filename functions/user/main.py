4a19440417c53a593578b300b86d6787179baa68
nikalosa <nikalosaberidze@gmail.com>
2020-09-03 23:14:47 +0400

    Add queries

--- functions/user/main.py
+++ functions/user/main.py
@@ -2,15 +2,70 @@ import json
 from google.cloud import bigquery
 
 
-def maximum_scores():
-    return {"maxScores": [{"voiceId": "123", "score": 0.8}, {"voiceId": "124", "score": 0.9}]}
+def maximum_scores(user_id):
+    client = bigquery.Client('speech-similarity')
+
+    query_job = client.query(
+        """
+            SELECT OriginalVoiceId,
+                   Max(score) as Score
+            FROM   statistics.recorded_voices
+            WHERE userid = '{userId}'
+            GROUP  BY originalvoiceid
+        """.format(userId=user_id)
+    )
+
+    maximum_scorers = query_job.result()
+    results = {"maxScorers": []}
+
+    for row in maximum_scorers:
+        voice_id = row.OriginalVoiceId
+        score = row.Score
+        results["maxScorers"].append({"voiceId": voice_id, "score": score})
+
+    return results
+    # return {"maxScores": [{"voiceId": "123", "score": 0.8}, {"voiceId": "124", "score": 0.9}]}
 
 
-def scores_by_time():
+def scores_by_time(user_id):
+    client = bigquery.Client('speech-similarity')
+
+    query_job = client.query(
+        """
+            select
+              OriginalVoiceId,
+              Score,
+              Datetime,
+              count
+              (
+                distinct(UserId)
+              ) as tried
+            from
+              statistics.recorded_voices
+            group by
+              OriginalVoiceId
+        """
+    )
     return {"timeScores": [{"voiceId": "123", "scores": [dict(datetime="12.05.21", score=0.1), dict(datetime="13.05.21", score=0)]}]}
 
 
 def popular_voices():
+    # client = bigquery.Client('speech-similarity')
+    #
+    # query_job = client.query(
+    #     """
+    #         select
+    #           OriginalVoiceId,
+    #           count
+    #           (
+    #             distinct(UserId)
+    #           ) as tried
+    #         from
+    #           statistics.recorded_voices
+    #         group by
+    #           OriginalVoiceId
+    #     """
+    # )
     return {"popular_voices": ["123", "124", "124"]}
 
 
